81b910ae5c009427658aaae287b3587f6119bb63
Sam Williams <68836604+smwllyms@users.noreply.github.com>
2024-12-15 10:29:32 -0500

    Generalized audio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
-import logo from './img/coyote/coyotes/logo.png';
+import logo from './img/coyote/coyotes/logo2.png';
 import './App.css';
 
+import * as React from "react";
+
 import AudioPlayer from './components/audioplayer';
-import song from "./audio/espanol.wav";
+// import song from "./audio/espanol.wav";
+
+import manifest from "./audio/manifest.json";
+
+const getSongFiles = async function () {
+
+  let result = []
+
+  for (const v of manifest) {
+    let songTrack = await import("./audio/" + v.file)
+
+    result.push({
+      title: v.title,
+      artist: v.artist,
+      song: songTrack.default
+    })
+  }
+
+  return result;
+  
+}
 
 function App() {
+
+  const [songFiles, setSongFiles] = React.useState([]);
+
+  React.useEffect(()=>{
+    async function load() {
+      const result = await getSongFiles();
+      setSongFiles(result)
+    }
+
+    load();
+  }, [setSongFiles])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <AudioPlayer audioSrc={song}/>
+        {
+          songFiles.map((v,i)=> {
+            return <AudioPlayer 
+              key={i} 
+              title={v.title} 
+              artist={v.artist}
+              audioSrc={v.song}/>
+          })
+        }
+        
       </header>
     </div>
   );
--- src/components/audioplayer.js
+++ src/components/audioplayer.js
@@ -1,28 +1,53 @@
-
-const audioPlayerContainerStyle = {
-    width:"50vw",
-    height:"40px",
-    background: "rgba(0,0,0,0.7)",
-    borderRadius: "25px",
-    padding:"10px"
-}
-
 const audioPlayerStyle = {
     width:"100%",
     height:"100%",
+    minHeight:"40px",
     background: "rgba(0,0,0,0)",
-    padding:"0px",
-    margin:"0px",
     borderRadius: "25px",
-    background: "black"
+    position: "relative",
+    marginTop: "0px"
     // filter:"invert()"
 }
+
+const frameStyle = {
+    marginTop: "5px",
+    width:"50vw",
+    minHeight:"40px",
+    background: "rgba(0,0,0,0.9)",
+    padding: "10px",
+    borderRadius: "25px",
+    display: "flex",
+    flexDirection: "column",
+    gap:"5px",
+    color:"white"
+}
+
+const metadataStyle = {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    paddingLeft: "8px"
+}
+
+const titleStyle = {
+    color: "white"
+};
+const artistStyle = {
+    paddingLeft: "8px",
+    color: "grey"
+};
 
 export default function AudioPlayer(props) {
 
     return (
-        <div className="audio-player-container" style={audioPlayerContainerStyle}>
+        <div style={frameStyle}>
+            <div style={metadataStyle}>
+                <span style={titleStyle}>{props.title}</span>
+                <span style={artistStyle}>{props.artist ? `\t- ${props.artist}` : ""}</span>
+            </div>
+
             <audio controls style={audioPlayerStyle} src={props.audioSrc}></audio>
         </div>
+        
     );
 }
